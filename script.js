f3f95b31c9a4c23f3e248c775ca2ef6346b630da
RochaGabriell <rochagabriell.dev@gmail.com>
2023-01-23 18:43:36 -0300

    the next level

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-    run: ['01-01', '01-02', '01-06', '01-08', '01-09', '01-10'],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if (dayExists) {
+        alert("Dia já incluso 🔴")
+        return
+    }
+
+    alert("Adicionado com sucesso ✅")
+    nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
+function save() {
+    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
 
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
 nlwSetup.load()
